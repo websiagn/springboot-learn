371215d644a5cadae03ba6c51eb71470eee9662f
youchen <1450207968@qq.com>
2020-07-24 15:05:26 +0800

    注册Servlet容器三大组件 —— filter Arrays.asList()将一个数组转换为一个List集合 Arrays.asList("/hello","/myServlet")等同于下面 String[] myArray = {"/hello", "/myServlet"} Arrays.asList(myArray)

--- src/main/java/com/example/springbootlearn/config/ServletConfig.java
+++ src/main/java/com/example/springbootlearn/config/ServletConfig.java
@@ -1,11 +1,15 @@
 package com.example.springbootlearn.config;
 
+import com.example.springbootlearn.filter.MyFilter;
 import com.example.springbootlearn.servlet.MyServlet;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class ServletConfig{
@@ -25,11 +29,22 @@ public class ServletConfig{
     }
 
     /**
-     * Servlet三大组件
+     * Servlet三大组件__servlet
      */
     @Bean
     public ServletRegistrationBean myServlet() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(), "/myServlet");
+        return registrationBean;
+    }
+
+    /**
+     * Servlet三大组件__filter
+     */
+    @Bean
+    public FilterRegistrationBean myFilter(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new MyFilter());
+        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
         return registrationBean;
     }
 }
