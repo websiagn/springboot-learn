467590ea0f6cd28b0be651a54938045b447b42d7
youchen <1450207968@qq.com>
2020-07-24 14:49:21 +0800

    注册Servlet容器三大组件 —— servlet

--- src/main/java/com/example/springbootlearn/config/ServletConfig.java
+++ src/main/java/com/example/springbootlearn/config/ServletConfig.java
@@ -1,13 +1,19 @@
 package com.example.springbootlearn.config;
 
+import com.example.springbootlearn.servlet.MyServlet;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServletConfig{
 
+    /**
+     * t通过WebServerFactoryCustomizer修改ervlet容器的配置
+     * @return
+     */
     @Bean
     public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer () {
         return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
@@ -16,5 +22,14 @@ public class ServletConfig{
                 factory.setPort(8081);
             }
         };
+    }
+
+    /**
+     * Servlet三大组件
+     */
+    @Bean
+    public ServletRegistrationBean myServlet() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(), "/myServlet");
+        return registrationBean;
     }
 }
