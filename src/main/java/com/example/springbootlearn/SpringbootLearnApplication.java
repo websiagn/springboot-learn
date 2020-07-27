aece6bb1a36b27dc24cc909a8de195600389c318
youchen <1450207968@qq.com>
2020-07-27 15:11:04 +0800

    使用MapperScan批量扫描所有的Mapper接口

--- src/main/java/com/example/springbootlearn/SpringbootLearnApplication.java
+++ src/main/java/com/example/springbootlearn/SpringbootLearnApplication.java
@@ -1,11 +1,14 @@
 package com.example.springbootlearn;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.sql.DataSource;
 
+//使用MapperScan批量扫描所有的Mapper接口
+@MapperScan(value = "com.example.springbootlearn.mapper")
 @SpringBootApplication
 public class SpringbootLearnApplication {
 
