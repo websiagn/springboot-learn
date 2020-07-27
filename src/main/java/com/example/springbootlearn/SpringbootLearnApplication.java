e942792a3a31c7d6e4dad3fec41274364f4b76c7
youchen <1450207968@qq.com>
2020-07-27 10:52:15 +0800

    添加JDBC数据源、连接数据库

--- src/main/java/com/example/springbootlearn/SpringbootLearnApplication.java
+++ src/main/java/com/example/springbootlearn/SpringbootLearnApplication.java
@@ -1,10 +1,15 @@
 package com.example.springbootlearn;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class SpringbootLearnApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootLearnApplication.class, args);
